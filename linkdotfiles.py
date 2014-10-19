1d6bc6a813cd6712cb8aff001713f9113ed3e91d
jon <jon@b4rg.de>
2014-10-19 13:40:45 +0200

    cleaning

--- linkdotfiles.py
+++ linkdotfiles.py
@@ -4,3 +4,8 @@ import os
 dirname, filename = os.path.split(os.path.abspath(__file__))
 filelist = os.listdir(dirname)
 print(filelist)
+
+for file in filelist:
+   if file[0] == '.' and '.swp' not in file:
+      print('ln -s'+dirname+'/'+file)
+      os.system('ln -s'+dirname+'/'+file+' '+'~/'+file)
